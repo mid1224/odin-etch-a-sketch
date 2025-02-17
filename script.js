ccee88cea5bf862b57473c9392455ac0e704c4f4
mid1224 <61130938+mid1224@users.noreply.github.com>
2025-02-17 20:19:23 +0700

    Create a 16x16 grid of square divs

---
+++ script.js
@@ -0,0 +1,6 @@
+for (let i = 1; i <= 16*16; i++)
+{
+    let newDiv = document.createElement("div");
+    newDiv.className = "grid";
+    document.getElementById("container").appendChild(newDiv);
+}
