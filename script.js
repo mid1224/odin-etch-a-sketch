bff8aff51ec92a29ef6a289c718a0aeadb3ea185
mid1224 <61130938+mid1224@users.noreply.github.com>
2025-02-17 20:38:31 +0700

    Change divs' color when the cursor passes it

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-for (let i = 1; i <= 16*16; i++)
+for (let i = 0; i < 16*16; i++)
 {
     let newDiv = document.createElement("div");
     newDiv.className = "grid";
     document.getElementById("container").appendChild(newDiv);
+}
+
+
+let gridItems = document.getElementsByClassName("grid");
+
+for (let j = 0; j < gridItems.length; j++)
+{
+    gridItems[j].addEventListener("mouseenter", function(event) {
+        event.target.style.background = "black";
+    })
 }
