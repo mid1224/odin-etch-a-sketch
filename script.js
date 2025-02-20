0857cb83f2bffc5f4f3758bb278bf0ead9174a3a
mid1224 <61130938+mid1224@users.noreply.github.com>
2025-02-20 20:47:25 +0700

    Finish the project
    Add a button to change the grid size, a button the reset the sketch; Add the title & polish the UI

--- script.js
+++ script.js
@@ -5,12 +5,79 @@ for (let i = 0; i < 16*16; i++)
     document.getElementById("container").appendChild(newDiv);
 }
 
-
 let gridItems = document.getElementsByClassName("grid");
 
-for (let j = 0; j < gridItems.length; j++)
+for (let i = 0; i < gridItems.length; i++)
 {
-    gridItems[j].addEventListener("mouseenter", function(event) {
+    gridItems[i].addEventListener("mouseenter", function(event) {
         event.target.style.background = "black";
     })
 }
+
+let settingButton = document.getElementById("setting-button");
+
+settingButton.addEventListener("click", () => {
+    let gridSize = 0;
+    gridSize = prompt("Number of square per sides (0-100): ");
+
+    if (gridSize > 100)
+    {
+        gridSize = 100;
+    }
+    else if (gridSize < 0)
+    {
+        gridSize = 1;
+    }
+
+    while(gridItems.length > 0)
+    {
+        gridItems[0].remove();
+    }
+
+    for (let i = 0; i < gridSize*gridSize; i++)
+    {
+        let newDiv = document.createElement("div");
+        newDiv.className = "grid";
+        document.getElementById("container").appendChild(newDiv);
+    }
+    
+    gridItems = document.getElementsByClassName("grid");
+    
+    for (let i = 0; i < gridItems.length; i++)
+    {
+        gridItems[i].addEventListener("mouseenter", function(event) {
+            event.target.style.background = "black";
+        })
+
+        gridItems[i].style = "flex: 1 0 calc(100% /" + gridSize.toString() + ")";
+    }
+})
+
+let resetButton = document.getElementById("reset-button");
+
+resetButton.addEventListener("click", () => {
+    let currentGridSize = gridItems.length;
+
+    while(gridItems.length > 0)
+    {
+        gridItems[0].remove();
+    }
+
+    for (let i = 0; i < currentGridSize; i++)
+    {
+        let newDiv = document.createElement("div");
+        newDiv.className = "grid";
+        document.getElementById("container").appendChild(newDiv);
+    }
+    
+    gridItems = document.getElementsByClassName("grid");
+    
+    for (let i = 0; i < gridItems.length; i++)
+    {
+        gridItems[i].addEventListener("mouseenter", function(event) {
+            event.target.style.background = "black";
+        })
+
+        gridItems[i].style = "flex: 1 0 calc(100% /" + Math.sqrt(currentGridSize) + ")";
+    }
+})
